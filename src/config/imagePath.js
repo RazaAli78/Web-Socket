920a3c834b4d8a966773afd911f395b2198448d4
RazaAli78 <91953106+RazaAli78@users.noreply.github.com>
2022-02-04 17:25:56 +0500

    Fix Bug

--- src/config/imagePath.js
+++ src/config/imagePath.js
@@ -3,4 +3,4 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-export const GET_IMAGE_PATH = process.env.NODE_ENV === "production"? "https://gift-commerence.herokuapp.com/uploads/images/":"http://localhost:8000/uploads/images";
+export const GET_IMAGE_PATH = process.env.NODE_ENV === "production"? "https://gift-commerence.herokuapp.com/uploads/images":"http://localhost:8000/uploads/images";
