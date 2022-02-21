534445e0324a717ed1b1bd91a60ee0ef0b6688ac
RazaAli78 <91953106+RazaAli78@users.noreply.github.com>
2022-02-21 13:32:28 +0500

    Update multer.js

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
- const GET_IMAGE_PATH = process.env.NODE_ENV === "production"? "https://recreave-mvp-backend.herokuapp.com/uploads/media/":`http://localhost:${process.env.PORT}/uploads/media`;
+ const GET_IMAGE_PATH = process.env.NODE_ENV === "production"? "https://recreave-mvp-backend.herokuapp.com/uploads/images/":`http://localhost:${process.env.PORT}/uploads/images`;
  export default GET_IMAGE_PATH;
